8b853c4d21ca8c3287a05328be985a89eb706e58
kaizen <ojkelvinchu@gmail.com>
2025-08-13 11:44:59 +0100

    Define API for task restarts, remove hardcoded IPs and URLs

--- lib/octopus_ping.ex
+++ lib/octopus_ping.ex
@@ -3,47 +3,30 @@ defmodule OctopusPing do
   Documentation for `OctopusPing`.
   """
 
-  def start(addresses, :ip) when is_list(addresses) do
+  def start(category, addresses) when is_list(addresses) do
     IO.puts("Start pinging all IPs in #{inspect(addresses)}.")
     start_worker(
       %{
-        addresses: [
-          "192.168.1.0/24",
-          "172.24.208.5",
-          "8.8.8.8",
-          "172.20.112.1",
-          "172.24.5.87",
-          "172.20.10.4",
-          "127.0.0.1"
-        ],
-        category: "IPs"
+        category: category,
+        addresses: addresses
       }
     )
   end
 
-  def start(urls, :app) when is_list(urls) do
-    IO.puts("Start curl requests to all site urls in #{inspect(urls)}.")
-    start_worker(
-      %{
-        addresses: [
-          "https://bbc.com",
-          "https://cnn.com",
-          "https://netflix.com"
-        ],
-        category: "Apps"
-      }
-    )
-  end
-
-  def start(_addresses) do
-    IO.puts("Error. Invalid value for addresses.")
+  def start(_) do
+    IO.puts("Error. Invalid argument value for addresses.")
   end
 
   def stop(name) do
     via_tuple(name)
-    |> GenServer.cast(:stop)
+    |> GenServer.cast(:stop_tasks)
 
     IO.puts("Stop monitoring #{name}.")
+  end
+
+  def restart(name) do
+    via_tuple(name)
+    |> GenServer.cast(:restart_tasks)
   end
 
   def get_live(name) do
