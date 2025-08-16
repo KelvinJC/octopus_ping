3503f350cd9754f5930f7755bc899622bf97065d
kaizen <ojkelvinchu@gmail.com>
2025-08-16 13:51:28 +0100

    Move task response functions to TaskUtil module for better code organisation

--- lib/octopus_ping/net_manager.ex
+++ lib/octopus_ping/net_manager.ex
@@ -1,6 +1,7 @@
 defmodule OctopusPing.NetManager do
   use GenServer
 
+  alias OctopusPing.TaskUtil
   alias OctopusPing.Tasks
   require Logger
 
@@ -84,13 +85,13 @@ defmodule OctopusPing.NetManager do
 
   # the request succeeds
   def handle_info({ref, {:ok, _msg}}, state) do
-    updated_tasks = process_task_response(ref, state, :successful)
+    updated_tasks = TaskUtil.process_task_response(ref, state, :successful)
     {:noreply, %{state | tasks: updated_tasks}}
   end
 
   # the request fails
   def handle_info({ref, {:error, _msg}}, state) do
-    updated_tasks = process_task_response(ref, state, :failed)
+    updated_tasks = TaskUtil.process_task_response(ref, state, :failed)
     {:noreply, %{state | tasks: updated_tasks}}
   end
 
@@ -100,83 +101,14 @@ defmodule OctopusPing.NetManager do
   end
 
   def handle_call(:get_successful, _from, state) do
-    hosts_or_urls = filter_by_status(:successful, state.tasks)
+    hosts_or_urls = TaskUtil.filter_by_status(:successful, state.tasks)
     {:reply, hosts_or_urls, state}
   end
 
   def handle_call(:get_failed, _from, state) do
-    hosts_or_urls = filter_by_status(:failed, state.tasks)
+    hosts_or_urls = TaskUtil.filter_by_status(:failed, state.tasks)
     {:reply, hosts_or_urls, state}
   end
 
   defp via_tuple(name), do: {:via, Registry, {OctopusPing.Registry, name}}
-
-  defp process_task_response(task_reference, state, task_status) do
-    # demonitor and flush task process.
-    Process.demonitor(task_reference, [:flush])
-
-    # query task info from state
-    task =
-      Enum.find(
-        state.tasks,
-        fn %{task: %{ref: ref_id}}  ->
-          ref_id == task_reference
-        end
-      )
-
-    # update task info in state
-    case task do
-      nil ->
-        state.tasks
-
-      _task_found ->
-        state.tasks
-        |> MapSet.delete(task)
-        |> MapSet.put(%{task | status: task_status})
-    end
-  end
-
-  @doc """
-  Extracts the hosts or URLs of tasks matching the given status.
-
-  ## Parameters
-    * `task_status` — the task status to match (e.g., `:successful`, `:failed`).
-    * `tasks` — the list of task maps stored in state. Each containing a `:status` key
-      and a `:host` or `:url` key
-
-  ## Returns
-  A list of hosts or URLs for all tasks whose status matches `task_status`.
-
-  ## Examples
-
-      iex> tasks = [
-      ...>   %{
-      ...>      host: "192.168.0.1",
-      ...>      status: :pending
-      ...>    },
-      ...>   %{
-      ...>      host: "127.0.0.1",
-      ...>      status: :successful
-      ...>    },
-      ...>   %{
-      ...>      host: "8.8.8.8",
-      ...>      status: :failed
-      ...>    }
-      ...> ]
-      iex> OctopusPing.NetManager.filter_by_status(:failed, tasks)
-      ["8.8.8.8"]
-  """
-  def filter_by_status(task_status, tasks) do
-    Enum.flat_map(
-      tasks,
-      fn %{status: status} = task ->
-        if status == task_status do
-          hosts_or_urls = Map.get(task, :host, Map.get(task, :url))
-          [hosts_or_urls]
-        else
-          []
-        end
-      end
-    )
-  end
 end
---
+++ lib/octopus_ping/task_util.ex
@@ -0,0 +1,70 @@
+defmodule OctopusPing.TaskUtil do
+  def process_task_response(task_reference, state, task_status) do
+    # demonitor and flush task process.
+    Process.demonitor(task_reference, [:flush])
+
+    # query task info from state
+    task =
+      Enum.find(
+        state.tasks,
+        fn %{task: %{ref: ref_id}}  ->
+          ref_id == task_reference
+        end
+      )
+
+    # update task info in state
+    case task do
+      nil ->
+        state.tasks
+
+      _task_found ->
+        state.tasks
+        |> MapSet.delete(task)
+        |> MapSet.put(%{task | status: task_status})
+    end
+  end
+
+  @doc """
+  Extracts the hosts or URLs of tasks matching the given status.
+
+  ## Parameters
+    * `task_status` — the task status to match (e.g., `:successful`, `:failed`).
+    * `tasks` — the list of task maps stored in state. Each containing a `:status` key
+      and a `:host` or `:url` key
+
+  ## Returns
+  A list of hosts or URLs for all tasks whose status matches `task_status`.
+
+  ## Examples
+
+      iex> tasks = [
+      ...>   %{
+      ...>      host: "192.168.0.1",
+      ...>      status: :pending
+      ...>    },
+      ...>   %{
+      ...>      host: "127.0.0.1",
+      ...>      status: :successful
+      ...>    },
+      ...>   %{
+      ...>      host: "8.8.8.8",
+      ...>      status: :failed
+      ...>    }
+      ...> ]
+      iex> OctopusPing.NetManager.filter_by_status(:failed, tasks)
+      ["8.8.8.8"]
+  """
+  def filter_by_status(task_status, tasks) do
+    Enum.flat_map(
+      tasks,
+      fn %{status: status} = task ->
+        if status == task_status do
+          hosts_or_urls = Map.get(task, :host, Map.get(task, :url))
+          [hosts_or_urls]
+        else
+          []
+        end
+      end
+    )
+  end
+end
